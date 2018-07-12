29b1875cdc3cf9deb9be73704afefabe9bb8fd8e
David Sarginson <david.sarginson@digital.hmrc.gov.uk>
2018-07-12 08:53:22 +0100

    fixing core

--- src/tdd_trainer/core.clj
+++ src/tdd_trainer/core.clj
@@ -17,6 +17,6 @@
 (defn -main
   "Starts the TDD Trainer server"
   [& args]
-  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080")) pfdock]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
     (info (str "Starting Server on port:" port " ..."))
     (reset! server (run-server #'site {:port port})))) 
