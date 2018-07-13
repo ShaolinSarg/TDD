a9b4aea7cb3999fe32918e0aab975664dcc4dc85
David Sarginson <david.sarginson@digital.hmrc.gov.uk>
2018-07-13 10:55:15 +0100

    adding validation to start time parameter for POST /session

--- src/tdd_trainer/routes/session.clj
+++ src/tdd_trainer/routes/session.clj
@@ -1,18 +1,36 @@
 (ns tdd-trainer.routes.session
   (:require [compojure.core :refer [defroutes POST]]
-            [ring.util.response :refer [content-type created response]]
+            [ring.util.response :refer [content-type created status response]]
             [taoensso.timbre :refer [debug info]]
-            [tdd-trainer.services.session :as service]))
+            [tdd-trainer.services.session :as service]
+            [clj-time.format :as f]))
 
 
-(defn- create-session []
+(def json-datetime-formatter (f/formatters :mysql))
+
+
+(defn validate-time [t]
+  (try
+    (f/parse json-datetime-formatter t)
+
+    (catch Exception e
+      (info (str "Invalid start time: " t))
+      nil)))
+
+
+(defn- create-session [start-time]
   (do
     (info "calling add session service")
-    (let [initial-session (service/create-session "")
-          session-id (:session-id initial-session)]
+    (if-let [valid-start (validate-time start-time)]
+      (let [initial-session (service/create-session "sdfsd")
+            session-id (:session-id initial-session)]
 
-      (-> (created (str "/session/" session-id))
-          (content-type "application/json")))))
+        (-> (created (str "/session/" session-id))
+            (content-type "application/json")))
+      (status (response "bad time") 400))))
 
 (defroutes session-routes
-  (POST "/session" [] (create-session)))
+  (POST "/session" request (let [t (get-in request [:body "timestamp"])]
+                               (info (str "time is :" t))
+                               (info (str "request is :" request))
+                               (create-session t))))
--- test/tdd_trainer/routes/session_test.clj
+++ test/tdd_trainer/routes/session_test.clj
@@ -1,21 +1,40 @@
 (ns tdd-trainer.routes.session-test
   (:require [tdd-trainer.routes.core :as sut]
             [clojure.test :refer [deftest testing is]]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock]
+            [tdd-trainer.routes.session :as routes]
+            [clj-time.core :as t]))
 
 (deftest session-route-tests
   (testing "endpoints should return a valid success response"
 
     (testing "POST /session"
       (testing "returns the correct status"
-        (is (= 201 (:status (sut/all-routes (mock/request :post "/session"))))))
+        (is (= 201 (:status (sut/site (-> (mock/request :post "/session")
+                                          (mock/json-body {:timestamp "2018-04-05 23:54:52"})))))))
 
       (testing "returns the correct content type"
         (is (= "application/json"
-               (get-in (sut/all-routes (mock/request :post "/session"))
+               (get-in (sut/site (-> (mock/request :post "/session")
+                                     (mock/json-body {:timestamp "2018-04-05 23:54:52"})))
                        [:headers "Content-Type"]))))
 
       (testing "returns a location header for the new resource"
         (with-redefs-fn {#'tdd-trainer.services.session/create-session (fn [t] {:session-id 400})}
-          #(is (= "/session/400" (get-in (sut/all-routes (mock/request :post "/session"))
-                                        [:headers "Location"]))))))))
+          #(is (= "http://localhost/session/400" (get-in (sut/site (-> (mock/request :post "/session")
+                                                       (mock/json-body {:timestamp "2018-04-05 23:54:52"})))
+                                         [:headers "Location"])))))))
+
+  (testing "endpoints should return a bad request response"
+    (testing "POST /session"
+      (testing "when the date time is invalid"
+        (is (= 400 (:status (-> (sut/site (mock/request :post "/session"))
+                                (mock/json-body {:timestamp "something"}))))))))
+  )
+
+(deftest validate-time-tests
+  (testing "return nil for invalid time"
+    (is (nil? (routes/validate-time "1234"))))
+  (testing "return a dateTime for a valid time"
+    (is (= (t/date-time 2018 3 5 13 34 45)
+           (routes/validate-time "2018-03-05 13:34:45")))))
